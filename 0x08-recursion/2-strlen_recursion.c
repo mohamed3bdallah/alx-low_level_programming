ac25b43e83ad0c3cbccdda16ff6ec5a7644ddc23
mohamed3bdallah <mohammedabdallah757@gmail.com>
2023-10-25 00:28:49 -0700

    3-factorial.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,20 @@
 #include "main.h"
+
 /**
- * _strlen_recursion - function that returns the length of a string
- *
- * @s:function parametar
- *
- * Return: length
- */
+  * _strlen_recursion - main function
+  *
+  * @s: Function parameter
+  *
+  * Return: Length
+  */
 int _strlen_recursion(char *s)
 {
+	int lnt = 0;
+
 	if (*s)
 	{
-		return (_strlen_recursion(s + 1));
-
-}
+	lnt++;
+	lnt += _strlen_recursion(s + 1);
+	}
+	return (lnt);
 }
